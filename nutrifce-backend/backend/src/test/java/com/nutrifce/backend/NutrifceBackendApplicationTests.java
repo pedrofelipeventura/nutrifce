a197c0e75ca5e51da8f3f9b0ce31718bfcf38d6d
MatheusDev622 <168672066+MatheusDev622@users.noreply.github.com>
2025-11-20 19:28:54 -0300

    Add files via upload

---
+++ nutrifce-backend/backend/src/test/java/com/nutrifce/backend/NutrifceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrifce.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrifceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
